286b4561d5b58892daa6c347b1e817ac828a470e
Aleksandar <amilinkovic.ns@gmail.com>
2018-11-13 12:12:59 +0100

    zadatak10

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var a = 5;
+
+var anonimaFunkcija = function(b) {
+   var c = b + a;
+
+    return c
+};
+
+console.log(anonimaFunkcija(5));
+
+function mojaFukcija(){
+    return posebna *a;
+}
+
+console.log(mojaFukcija());
+console.log(anonimaFunkcija(variablaPre));
+console.log(anonimaFunkcija(variablaPosle));
+
+
+
